bcf97b93de0505bd779bc89e3b4314f1b019660a
Biruk Ayalew <61549951+birukayalew@users.noreply.github.com>
2021-01-05 06:16:48 -0800

    Update CountingSort.py

--- Week2/CountingSort.py
+++ Week2/CountingSort.py
@@ -2,7 +2,6 @@ def CountingSort1(arr):
     #finding the maximum array size
     max1=0
     l=[]
-    s=""
     for i in range(len(arr)):
         if max1<arr[i]:
             max1=arr[i]
@@ -16,12 +15,9 @@ def CountingSort1(arr):
     #printing the result
     for i in range(len(l)):
         for j in range(l[i]):
-            print(s+str(i),end=" ")
-    return s.rstrip(' ')
-#print(CountingSort1([4,3,2,2,1]))
-result=CountingSort1([4,3,2,2,1])
-ans=' '.join(map(int, int(result)))
-print(ans)
+            print(i,end=" ")
+CountingSort1([4,3,2,2,1])
+
       
 
     
