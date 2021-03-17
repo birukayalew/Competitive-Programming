22785b48fea49144a1e241c182b7aad2e915e4da
Biruk Ayalew <61549951+birukayalew@users.noreply.github.com>
2021-03-17 21:30:03 +0300

    Create simplify_path.py

---
+++ Week10/simplify_path.py
@@ -0,0 +1,13 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        
+        split_path = [x for x in path.split('/') if x != "" and x != "."]
+        answer = []
+        for character in split_path:
+            if character == "..":
+                if answer:
+                    answer.pop()
+            else:
+                answer.append(character)
+        return "/" + "/".join(answer)
+        
