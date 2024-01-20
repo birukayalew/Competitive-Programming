7d7bc0ee17b8a703a572d97b648204b221ae68b3
Biruk Tadesse <61549951+birukayalew@users.noreply.github.com>
2024-01-20 12:35:07 +0300

    Time: 474 ms (76.71%), Space: 28.1 MB (87.33%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,8 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        seen = {}
+        seen = set()
         for i in range(len(nums)):
-            if nums[i] in seen and i - seen[nums[i]] <= k:
-                return True
-            seen[nums[i]] = i
+            if nums[i] in seen:
+                return True  # Duplicate found within k distance
+
+            seen.add(nums[i])
+
+            if len(seen) > k:
+                seen.remove(nums[i - k])  # Remove element to keep window size at k
+
         return False
