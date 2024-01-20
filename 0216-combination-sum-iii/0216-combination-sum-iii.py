887559c375de351e35e96cfb15cb2a012cae8f47
Biruk Tadesse <61549951+birukayalew@users.noreply.github.com>
2024-01-20 22:02:37 +0300

    Time: 46 ms (23.72%), Space: 16.4 MB (64.37%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,15 +1,19 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        def backtrack(comb, remain, start):
-            if remain == 0 and len(comb) == k:
+        def backtrack(comb, remain, start_num):
+            if remain == 0 and len(comb) == k:  # Condition: k numbers sum up to n
                 res.append(list(comb))
                 return
-            elif remain < 0 or len(comb) == k:
+
+            elif remain < 0 or len(comb) == k:  # Exceeded target or exceeded number limit
                 return
 
-            for i in range(start, 10):  # Use numbers from 1 to 9
+            for i in range(start_num, 10):  # Numbers from 1 to 9
+                if i in comb:
+                    continue
+                    
                 comb.append(i)
-                backtrack(comb, remain - i, i + 1)  # Next number must be larger than i
+                backtrack(comb, remain - i, i + 1)
                 comb.pop()
             
         res = []
