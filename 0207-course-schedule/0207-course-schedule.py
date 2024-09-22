9b8cde4e69a8687b7dabe027bd2197323221ec5c
Biruk Tadesse <61549951+birukayalew@users.noreply.github.com>
2024-09-22 15:19:09 -0400

    Time: 113 ms (8.25%), Space: 20.4 MB (5.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,27 +2,23 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
         visited = set()
-        visiting = set()
-        for ai, bi in prerequisites:
-            graph[bi].append(ai)
-
-        def has_cycle(course, visited, visiting):
-            if course in visiting: 
-                return True  
-            if course in visited: 
+        for c1, c2 in prerequisites:
+            graph[c2].append(c1)
+            
+        @cache
+        def dfs(c):
+            res = True
+            visited.add(c)
+            for neighbour in graph[c]:
+                if neighbour in visited:
+                    return False
+                res = res and dfs(neighbour)
+            visited.remove(c)
+            return res
+        
+        for c in range(numCourses):
+            res = dfs(c)
+            if not res:
                 return False
-
-            visiting.add(course)  # Move this line BEFORE the loop
-
-            for neighbor in graph[course]: 
-                if has_cycle(neighbor, visited, visiting): 
-                    return True  
-
-            visiting.remove(course)  
-            visited.add(course) 
-            return False 
-       
-        for course in range(numCourses):
-            if course not in visited and has_cycle(course, visited, visiting):
-                return False
+            
         return True
