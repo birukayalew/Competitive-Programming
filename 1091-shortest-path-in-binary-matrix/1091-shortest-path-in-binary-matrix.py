eaffa85070d8c057e60eea030b2936bf47b13f66
Biruk Tadesse <61549951+birukayalew@users.noreply.github.com>
2024-09-27 21:59:01 -0400

    Time: 451 ms (49.95%), Space: 18.2 MB (38.54%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,28 +1,19 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        # instantiate the directions
         directions = [(0, 1),(0, -1),(1, 0),(-1, 0),(1, 1),(1, -1),(-1, 1),(-1,-1)]
-        heap = []
-        visited = set()
-        if grid[0][0] == 0:
-            # move, i, j
-            heappush(heap, ((1, 0, 0)))
-                     
-        
-        while heap:
-            move, i, j = heappop(heap)
-            
-            if (i, j) == (n - 1, n - 1):
+        visited = set([(0, 0)])
+        q = deque([(1, 0, 0)])
+        n = len(grid)
+        if grid[0][0] == 1:
+            return -1
+        while q:
+            move, i, j = q.popleft()
+            if (i, j) == (n-1, n-1):
                 return move
             
-            if (i, j) not in visited:
-                visited.add((i, j))
-                for d in directions:
-                    new_i = i + d[0]
-                    new_j = j + d[1]
-                    
-                    if 0 <= new_i < n and 0 <= new_j < n and (new_i, new_j) not in visited and grid[new_i][new_j] == 0:
-                        heappush(heap, (move + 1, new_i, new_j))
-                        
-        return -1
+            for dx, dy in directions:
+                new_i, new_j = i + dx, j + dy
+                if 0 <= new_i < n and 0 <= new_j < n and grid[new_i][new_j] == 0 and (new_i, new_j) not in visited:
+                    visited.add((new_i, new_j))
+                    q.append((move + 1, new_i, new_j))
+        return -1        
